32e4952c8d8531d5b59cee8e60bdb9f1886a30fb
Oscar Bendicho <oscar.bendicho@aquitureforma.com>
2022-12-09 08:43:37 +0100

    Format

--- webpack.config.js
+++ webpack.config.js
@@ -1,97 +1,96 @@
-const path = require('path')
+const path = require('path');
 
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const WebpackBar = require('webpackbar')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const WebpackBar = require('webpackbar');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const mode = process.env.NODE_ENV || 'development';
 const prod = mode === 'production';
 
 module.exports = {
-    
-    target: 'web',
+  target: 'web',
 
-    entry: {
-        main: path.resolve(__dirname, './src/main.js')
+  entry: {
+    main: path.resolve(__dirname, './src/main.js'),
+  },
+
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].[hash].bundle.js',
+    chunkFilename: '[name].[hash].[id].js',
+  },
+
+  mode,
+
+  devtool: prod ? false : 'source-map',
+
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, './public'),
     },
-    output: {
-        path: path.resolve(__dirname, './dist'),
-        filename: '[name].[hash].bundle.js',
-        chunkFilename: '[name].[hash].[id].js'
+    historyApiFallback: true,
+    compress: true,
+    hot: true,
+    port: 5000,
+  },
+
+  resolve: {
+    alias: {
+      svelte: path.resolve('node_modules', 'svelte'),
     },
+    extensions: ['.mjs', '.js', '.svelte'],
+    mainFields: ['svelte', 'browser', 'module', 'main'],
+  },
 
-    mode,
-
-    devtool: prod ? false : 'source-map',
-
-    devServer: {
-        static: {
-            directory: path.resolve(__dirname, './public'),
+  module: {
+    rules: [
+      {
+        test: /\.svelte$/,
+        use: {
+          loader: 'svelte-loader',
+          options: {
+            compilerOptions: {
+              dev: !prod,
+            },
+            emitCss: prod,
+            hotReload: !prod,
+          },
         },
-        historyApiFallback: true,
-        compress: true,
-        hot: true,
-        port: 5000,
-    },
-
-    resolve: {
-        alias: {
-            svelte: path.resolve('node_modules', 'svelte')
+      },
+      {
+        // Required to prevent errors from Svelte on Webpack 5+
+        test: /node_modules\/svelte\/.*\.mjs$/,
+        resolve: {
+          fullySpecified: false,
         },
-        extensions: ['.mjs', '.js', '.svelte'],
-        mainFields: ['svelte', 'browser', 'module', 'main']
-    },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              url: false, // Necessary if you use url('/path/to/some/asset.png|jpg|gif')
+            },
+          },
+        ],
+        sideEffects: true,
+      },
+    ],
+  },
 
-    module: {
-        rules: [
-			{
-				test: /\.svelte$/,
-				use: {
-					loader: 'svelte-loader',
-					options: {
-						compilerOptions: {
-							dev: !prod
-						},
-						emitCss: prod,
-						hotReload: !prod
-					}
-				}
-			},
-            {
-				// Required to prevent errors from Svelte on Webpack 5+
-				test: /node_modules\/svelte\/.*\.mjs$/,
-				resolve: {
-					fullySpecified: false
-				}
-			},
-			{
-				test: /\.css$/,
-				use: [
-					{ 
-                        loader: MiniCssExtractPlugin.loader
-                    },
-					{ 
-                        loader: 'css-loader',
-                        options: {
-                            url: false, // Necessary if you use url('/path/to/some/asset.png|jpg|gif')
-                        }
-                    }
-				],
-                sideEffects: true
-			}
-			
-		]
-    },
-
-    plugins: [
-        new HtmlWebpackPlugin({ 
-            template: path.resolve(__dirname, './src/index.html') 
-        }) ,
-        new MiniCssExtractPlugin({
-            filename: '[name].[hash].css'
-        }),
-        new WebpackBar(),
-        new CleanWebpackPlugin(),
-    ]
-}
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/index.html'),
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[hash].css',
+    }),
+    new WebpackBar(),
+    new CleanWebpackPlugin(),
+  ],
+};
